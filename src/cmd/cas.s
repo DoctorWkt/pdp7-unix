7743f613d34de209e228471eef4aca43744239ee
Phil Budne <phil@ultimate.com>
2016-03-24 00:44:22 -0400

    split cas.s into cas.s and cas.x; get cas.s to assemble; add comments

--- src/cmd/cas.s
+++ src/cmd/cas.s
@@ -1,19 +1,21 @@
 "** 04-cas.pdf page 1
 " cas
 
+	" Usage: cas [ input [ output ] ]
+
    narg = i 017777
 
    lac 017777
    tad d5
    dac name1
    tad d4
-   dac name 2
-   lac narg
-   sad d4
-   jmp 1f
-   sad d8
-   jmp 2f
-   jmp 3f
+   dac name2
+   lac narg			" get arg count
+   sad d4			" 4?
+   jmp 1f			"  yes: default both to tty
+   sad d8			" 8?
+   jmp 2f			"  no: default input to tty
+   jmp 3f			" yes: use files from command line
 1:
    law ttyout
    dac name1
@@ -100,20 +102,20 @@ adv1:
    lac char
    sad o12
    jmp advanc
-   sad o72
-   skp
-   jmp 1f
+   sad o72			" ':'?
+   skp				" yes
+   jmp 1f			"  no
    -4
    dac labflg
    dac mod3
    jms wbuf "???
    lac lbufp
    dac 11
-   lac o170072
+   lac o170072			" "x:"
    dac i 10
-   lac o12
+   lac o12			" \n
    dac i 10
-   lac o60
+   lac o60			" 0
    dac i 10
    isz nwds
    isz nwds
@@ -127,14 +129,14 @@ adv1:
    dzm vis
    jmp advanc
 1:
-   sad o166
+   sad o166			" 'v'?
    skp
    jmp 1f
    lac visbit
    dac vis
    jmp advanc
 1:
-   sad 0162
+   sad o162			" 'r'?
    skp
    jmp letr
    isz mod3
@@ -160,7 +162,7 @@ adv1:
    isz nwds
    jmp 1b
 1:
-   lac o75170
+   lac o75170			" =x
    dac i 10
    isz nwds
    lac ob1
@@ -301,7 +303,7 @@ a:
    cma
    tad d1
 1:
-   tad dlex
+   tad delx
    tad dm4
    sma
    cla
@@ -400,7 +402,7 @@ wbuf: 0
    lac nwds
    dac 1f
    lac d3
-   sys write; buf; 1f: 0; "???
+   sys write; buf; 1: 0;	" PLB: label was 1f:
    dzm nwds
    lac bufp
    dac 10
@@ -422,7 +424,7 @@ sp: 053060
 o60: 060
 o60060: 060060
 o73: 073
-d6: 6 "seems like a dupe
+"d6: 6 "seems like a dupe
 d14: 14
 dm14: -14
 dm16: -16
@@ -481,579 +483,10 @@ tmp: .=.+1
 buf: .=.+500
 bufp: buf-1
 lbuf: .=.+10
-lpbufp: lbuf-1
+lbufp: lbuf-1
 nwds: .=.+1
-o1770072: 0170072
+o170072: 0170072
 nins: .=.+1
 o100: 0100
 o12012: 012012
 o12060: 012060
-
-
-
-
-
-"** 04-cas.pdf page 9
-:ca
-v
-af
-nl "???
-x
-hd
-v
-hi
-x
-mn
-r
-
-:cb
-x
-ga
-v
-gi
-ik
-kk
-mi "???
-ma
-aa
-ai
-ck
-ek
-gi
-x
-mn
-r
-
-:cc
-x
-kk
-v
-mi "???
-mc
-ka
-ca
-ac
-ai
-dl
-x
-mn
-r
-
-:cd
-v
-aa
-ai
-ck
-kk
-mi
-ma
-x
-mn
-r
-
-:ce
-x
-mk
-v
-ma
-"** 04-cas.pdf page 10
-aa
-ak
-x
-gh
-v
-ga
-x
-mn
-r
-
-:cf
-v
-aa
-ak
-x
-gh
-v
-ga
-x
-mn
-r
-
-:cg
-x
-gf
-v
-gk
-nk
-x
-kk
-v
-mi
-mc
-ka
-ca
-ac
-ai
-dl
-x
-mn
-r
-
-:ch
-x
-aa
-v
-na
-x
-gb "???
-v
-gk
-x
-ak
-v
-nk
-x
-mn
-r
-
-:ci
-x
-"** 04-cas.pdf page 11
-md
-v
-mi
-x
-lf
-v
-af
-x
-ad
-v
-ai
-x
-mn
-r
-
-:cj
-x
-ka
-v
-mc
-mg
-ki
-ai "???
-x
-ag
-v
-al "???
-x
-mn
-r
-
-:ck
-x
-aa
-v
-na
-x
-ak
-v
-ia
-x
-mk
-v
-fe
-x
-mn
-r
-
-:cl
-x
-aa
-v
-ma
-mk
-x
-mn
-r
-
-:cm
-v
-aa
-"** 04-cas.pdf page 12
-hf
-ak
-nk
-x
-mn
-r
-
-:cn
-x
-ak
-v
-mk
-aa
-na
-x
-mn
-r
-
-:co
-x "???
-ai
-v
-ch
-ei
-ck
-ai
-ac
-ca
-ka
-mc "???
-mi
-kk
-ck
-x
-mn
-r
-
-:cp
-v
-aa
-ai
-ck
-ek
-gi
-ga
-x
-mn
-r
-
-:cq
-x
-mc
-v
-ka
-ca
-ac
-ai
-ck
-kk
-mi
-mc
-"** 04-cas.pdf page 13
-x
-jh
-v
-nl
-x
-mn
-r
-
-:cr
-v
-aa
-ai
-ck
-ek
-gi
-ga
-x
-mk
-v
-gf
-x
-mn
-r
-
-:cs
-x
-ka
-v
-mc
-mi
-kk
-ik
-gi
-gc
-ea
-ca
-ac
-ai
-dl
-x
-mn
-r
-
-:ct
-x
-aa
-v
-al
-x
-mf
-v
-af
-x
-mn
-r
-
-:cu
-x
-aa
-v
-ka
-"** 04-cas.pdf page 14
-mc
-ml "???
-kk
-x
-ak
-v
-lk
-x
-mn
-r
-
-:cv
-x
-aa
-v
-mf
-x
-ak
-v
-mf
-x
-mn
-r
-
-:cw
-x
-aa
-v
-mc
-af
-x
-ak
-v
-mi
-af
-x
-mn
-r
-
-:cx
-v
-gf
-x
-ak
-v
-gf
-x
-aa
-v
-nl
-x
-mn
-r
-
-:cy
-x
-aa
-v
-if
-x
-ak
-"** 04-cas.pdf page 15
-v
-if
-nf
-x
-mn
-r
-
-:cz
-x
-aa
-v
-ak
-ma
-ml
-x
-mn
-r
-
-:c0
-x
-ck
-v
-ai
-ac
-ca
-ka
-mc
-mi
-kk
-ck
-x
-mn
-r
-
-:c1
-x
-ce
-v
-af
-mf
-x
-md
-v
-mi
-x
-mn
-r
-
-:c2
-x
-ca
-v
-ac
-ai
-ck
-ek
-gi
-gc
-ia
-ma
-ml "???
-"** 04-cas.pdf page 16
-mc
-mi
-kk
-hk
-fi
-fc
-ha
-x
-mn
-r
-
-:c7
-x
-aa
-v
-ak
-nb
-x
-mn
-r
-
-:c8
-x
-gc
-v
-ea
-ca
-ac
-ai
-ck
-ek
-gi
-gc
-ia
-ka
-mc
-mi
-kk
-ik
-gi
-x
-mn
-r
-
-:c9
-x
-ka
-v
-mc
-mi
-kk
-ck
-ai
-ac
-ca
-fa
-hc
-hi
-fk
-x
-mn
-"** 04-cas.pdf page 17
-x
-mn
-r
-
-:c3
-x
-aa
-v
-ak
-fe
-fi
-hk
-kk
-mi
-mc
-x
-ka
-v
-nd
-x
-mn
-r
-
-:c4
-x
-mh
-v
-ah
-ia
-ll
-x
-mn
-r
-
-:c5
-x
-ak
-v
-aa
-fa
-fi
-hk
-kk
-mi
-mc
-x
-ka
-v
-nd
-x
-mn
-r
-
-:c6
-x
-ck
-v
-ai
-ac
-ca
-ka
-
-" The end of file seems abrupt, every other :cX section ends with:
-" x
-" mn
-" r
