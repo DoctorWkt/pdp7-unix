76deea0152a0ece1754d07e9a3456a52e115bdda
Tom Everett <tom@khubla.com>
2019-10-19 12:15:28 -0600

    added p to build and image

---
+++ src/cmd/p0.s
@@ -0,0 +1,37 @@
+"** 10-36-55.pdf page 1
+osc = 0640001
+omq = 0640002
+otq = 0642000
+cmq = 0640004
+lmq = 0652000
+ecla = 0641000
+
+i = 020000
+
+save = 1
+getuid = 2
+open = 3
+read = 4
+write = 5
+creat = 6
+seek = 7
+tell = 8
+close = 9
+link = 10
+unlink = 11
+setuid = 12
+rename = 13
+exit = 14
+time = 15
+intrp = 16
+chdir = 17
+chmode = 18
+chowner = 19
+sysloc = 21
+capt = 23
+rele = 24
+status = 25
+sleep = 26
+smes = 27
+rmes = 28
+fork = 29
