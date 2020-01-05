5e0c12843f9f51d3677ef49bd23c69154589ee4b
phil <phil@ultimate.com>
2020-01-05 16:09:42 -0500

    more character table tinkering Combine cmd/scope.cas cmd/cas.cas into sys/cas.in Add src/other/chrtbl.s

---
+++ src/other/chrtbl.s
@@ -0,0 +1,28 @@
+" PLB 2020-01-05
+" start of a file to assemble output of cas.s
+
+" where data will be loaded, but immaterial
+" since cas output subtracts "b" from all addresses?
+. = 06000
+
+" base. maybe AFTER table??
+b:
+
+" 128-word dispatch table for ASCII
+
+null; null; null; null; null; null; null; null
+null; null; null; null; null; null; null; null
+null; null; null; null; null; null; null; null
+null; null; null; null; null; null; null; null
+space; excla; quote; number; dollar; percent; amper; accl
+lparn; rparn; aster; plus; comma; minus; period; sla
+c0; c1; c2; c3; c4; c5; c6; c7
+c8; c9; colon; semcln; less; equal; great; quest
+at; ca; cb; cc; cd; ce; cf; cg
+ch; ci; cj; ck; cl; cm; cn; co
+cp; cq; cr; cs; ct; cu; cv; cw
+cx; cy cz; lbk; bsla; rbk; cmflx; under
+accr; la; lb; lc; ld; le; lf; lg
+lh; li; lj; lk; ll; lm; ln; lo
+lp; lq; lr; ls; lt; lu; lv; lw
+lx; ly lz; lbr; or; rbr; tild; null
