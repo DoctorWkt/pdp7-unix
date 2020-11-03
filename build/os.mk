05ecb325bcb49e7b2a046bea00f955efcff6b096
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2020-11-03 07:23:16 -0500

    Support for OpenBSD and adJ

--- build/os.mk
+++ build/os.mk
@@ -9,6 +9,10 @@ ifeq ($(UNAME), Darwin)
 else 
 ifeq ($(UNAME), FreeBSD)
 	UNAME=FREEBSD
+else 
+ifeq ($(UNAME), OpenBSD)
+	UNAME=OPENBSD
+endif
 endif
 endif
 endif
@@ -23,6 +27,10 @@ ifeq ($(UNAME), FREEBSD)
 # FreeBSD
 	CC=cc
 	MAKE=gmake
+ifeq ($(UNAME), OPENBSD)
+# OpenBSD
+	CC=cc
+	MAKE=gmake
 else
 ifeq ($(UNAME), DARWIN)
 # Mac OS X
@@ -30,6 +38,7 @@ ifeq ($(UNAME), DARWIN)
 	MAKE=make
 else
 	$(error "Unknown OS: " $(UNAME))
+endif
 endif
 endif
 endif
