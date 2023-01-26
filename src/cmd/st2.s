0d5675f411c8ccf2ef503017f959f2f02412ad24
Mohamed Akram <mohd.akram@outlook.com>
2023-01-27 03:33:36 +0400

    Annotate st

--- src/cmd/st2.s
+++ src/cmd/st2.s
@@ -9,32 +9,32 @@ absv: 0
 	dzm absy+1
 	dzm absy+2
 1:
-	dac absi
+	dac absi	" absi = AC
 	sna
-	jmp i absv
+	jmp i absv	" while (absi) {
 	tad fppar
 	dac 9f+t
-	jms invert
+	jms invert	" invert(absi) -> ftmp1 = px[absi], ftmp2 = py[absi]
 	fld; ftmp1
 	fng
 	fad; absx
-	fst; absx
+	fst; absx	" absx -= ftmp1
 	fld; ftmp2
 	fng
 	fad; absy
-	fst; absy
-	jms invert
+	fst; absy	" absy -= ftmp2
+	jms invert	" invert(absi) -> ftmp1 = px[absi], ftmp2 = py[absi]
 	fld; ftmp1
 	fad; absx
-	fst; absx
+	fst; absx	" absx += ftmp1
 	fld; ftmp2
 	fad; absy
-	fst; absy
+	fst; absy	" absy += ftmp2
 	lac absi
 	tad ppar
 	dac 9f+t
-	lac i 9f+t
-	jmp 1b
+	lac i 9f+t	" absi = ppar[absi]
+	jmp 1b		" }
 
 invert: 0
 	lac i 9f+t
@@ -50,12 +50,12 @@ invert: 0
 	dac 4f
 	fld; 1:..
 	fng
-	fst; 2:..
-	jms updpln
+	fst; 2:..	" pww[par] = -pww[par]
+	jms updpln	" updpln()
 	fld; 3:..
-	fst; ftmp1
+	fst; ftmp1	" ftmp1 = px[par]
 	fld; 4:..
-	fst; ftmp2
+	fst; ftmp2	" ftmp2 = py[par]
 	jmp i invert
 
 	t = t+1
@@ -82,122 +82,122 @@ updpln: 0
 	dac 8f
 
 	fld; 1:..
-	fst; ftmp1
+	fst; ftmp1	" ftmp1 = px[fcplan]
 	fmp; 2:..
-	fst; ftmp2
+	fst; ftmp2	" ftmp2 = ftmp1 * pw[fcplan]
 	fld; 3:..
 	fmp; 4:..
 	fng
 	fad; ftmp2
-	fst; 5:..
+	fst; 5:..	" px[fcplan] = - (py[fcplan] * pww[fcplan]) + ftmp2
 	fld; 6:..
 	fmp; 7:..
-	fst; ftmp2
+	fst; ftmp2	" ftmp2 = py[fcplan] * pw[fcplan]
 	fld; ftmp1
 	fmp; 8:..
 	fad; ftmp2
-	fst; 0:..
+	fst; 0:..	" py[fcplan] = ftmp1 * pww[fcplan] + ftmp2
 	jmp updpln i
 
-updacc: 0
+updacc: 0		" update acceleration
 	lac cplan
 	sad par
-	jmp upda2
-	jms absxy
+	jmp upda2	" if (cplan != par) {
+	jms absxy	" absxy(cplan)
 	fld; absx
-	fad; shipx
-	jmp 1f
+	fad; shipx	" FAC = absx + shipx
+	jmp 1f		" } else {
 upda2:
 	fld; x
-	fng
-1:
-	fst; absx
+	fng		" FAC = -x
+1:			" }
+	fst; absx	" absx = FAC
 	fmp; absx
-	fst; ftmp1
+	fst; ftmp1	" ftmp1 = absx * absx
 	lac cplan
 	sad par
-	jmp 1f
+	jmp 1f		" if (cplan != par)
 	fld; absy
-	fad; shipy
+	fad; shipy	" FAC = absy + shipy
 	jmp 2f
 1:
 	fld; y
-	fng
+	fng		" else FAC = -y
 "** 12-92-119.pdf page 8
 2:
 	fst; absy
 	fmp; absy
 	fad; ftmp1
-	fst; dtmp1
+	fst; dtmp1	" dtmp1 = absy * absy + ftmp1
 	sqrt
-	fst; dpar
+	fst; dpar	" dpar = sqrt(dtmp1)
 	lac cplan
 	sad par
 	skp
-	jmp upda5
+	jmp upda5	" if (cplan == par) {
 	fld; ox
 	fng
 	fad; x
-	fst; ftmp1
+	fst; ftmp1	" ftmp1 = -ox + x
 	fmp; y
-	fst; horizv
+	fst; horizv	" horizv = ftmp1 * y
 	fld; y
 	fng
 	fad; oy
-	fst; ftmp2
+	fst; ftmp2	" ftmp2 = -y + oy
 	fmp; x
 	fad; horizv
 	fdv; dpar
-	fst; horizv
+	fst; horizv	" horizv = (ftmp2 * x + horizv) / dpar
 	fld; dpar
 	fcp; rpar
 	sma
-	jmp upda5
+	jmp upda5	" if (dpar < rpar) {
 	lac lanflg
 	spa
-	jmp upda5
+	jmp upda5	" if (!lanflg) {
 	fld; ftmp1
 	fmp; ftmp1
-	fst; ftmp1
+	fst; ftmp1	" ftmp1 *= ftmp1
 	fld; ftmp2
 	fmp; ftmp2
 	fad; ftmp1
 	fcp; crash
 	spa
-	jmp 1f
+	jmp 1f		" if (ftmp2 * ftmp2 + ftmp1 > crash) {
 	lac dhalt
-	dac goflg
-	dac crflg
-1:
+	dac goflg	" goflg = true
+	dac crflg	" crflg = true
+1:			" }
 	lac dhalt
-	dac lanflg
+	dac lanflg	" lanflg = true
 	fld; rpar
 	fdv; dpar
-	fst; ftmp1
+	fst; ftmp1	" ftmp1 = rpar / dpar
 	fmp; x
-	fst; x
-	fst; ox
+	fst; x		" x *= ftmp1
+	fst; ox		" ox = x
 	fld;  ftmp1
 	fmp; y
-	fst; y
-	fst; oy
+	fst; y		" y *= ftmp1
+	fst; oy		" oy = y
 	lac par
-	jms absxy
-	jms shipxy
-	jmp upda2
+	jms absxy	" absxy(par)
+	jms shipxy	" shipxy()
+	jmp upda2	" } } }
 "** 12-92-119.pdf page 9
 upda5:
 	fcp; fardst
 	spa
-	jmp 1f
+	jmp 1f		" if (dpar > fardst) {
 	lac cplan
 	sna
-	jmp 1f
+	jmp 1f		" if (cplan) {
 	lac dhalt
-	dac grvflg
-	jmp i updacc
-1:
-	dzm grvflg
+	dac grvflg	" grvflg = true
+	jmp i updacc	" return
+1:			" } }
+	dzm grvflg	" grvflg = false
 	lac fcplan
 	tad accl
 	dac 1f
@@ -205,18 +205,18 @@ upda5:
 	fdv; dtmp1
 	fcp; maxa
 	spa
-	jmp 2f
-	fst; maxa
+	jmp 2f		" if (accl[fcplan] / dtmp1 > maxa) {
+	fst; maxa	" maxa = accl[fcplan] / dtmp1
 	lac cplan
-	dac maxj
-2:
+	dac maxj	" maxj = cplan
+2:			" }
 	fdv; dpar
-	fst; ftmp1
+	fst; ftmp1	" ftmp1 = (accl[fcplan] / dtmp1) / dpar
 	fmp; absx
 	fad; ax
-	fst; ax
+	fst; ax		" ax += ftmp1 * absx
 	fld; ftmp1
 	fmp; absy
 	fad; ay
-	fst; ay
+	fst; ay		" ay += ftmp1 * absy
 	jmp i updacc
--- src/cmd/st3.s
+++ src/cmd/st3.s
@@ -7,21 +7,21 @@ updshp: 0
 	jmp .+4
 	lac bacflg
 	sma
-	jmp 3f
-	fld; ascale
+	jmp 3f		" if (forflg || bacflg) {
+	fld; ascale	" FAC = ascale
 	lac  forflg
 	sma
 	jmp 1f
 	lac bacflg
 	sma
 	jmp 1f+1
-	fld; fpzero
+	fld; fpzero	" if (forflg && bacflg) FAC = 0
 	jmp 2f
 1:
-	fng
+	fng		" if (!forflg) FAC = -FAC
 	lac scale
 	tad aexp
-	dac aexp
+	dac aexp	" FAC = FAC * 2**scale
 	lac forflg
 	sma
 	jmp .+3
@@ -29,89 +29,89 @@ updshp: 0
 	lac accflg	"[arrow pointing here from the text "how to set?" - scan markup]
 	sma
 	jmp .+3
-	fad; maxa
-	fng
+	fad; maxa	" if (accflg) FAC += maxa
+	fng		" FAC = -FAC
 	fst; ftmp1
 	fmp; ctheta
 	fad; ax
-	fst; ax
+	fst; ax		" ax += ftmp1 * ctheta
 	fld; ftmp1
 	fmp; stheta
 	fad; ay
-	fst; ay
-3:
+	fst; ay		" ay += ftmp1 * stheta
+3:			" }
 	fld; ox
 	fng
 	fad; ax
-	fst; ftmp1
+	fst; ftmp1	" ftmp1 = -ox + ax
 	fld; x
 	lac aexp
 	tad d1
 	dac aexp
 	fad; ftmp1
-	fst; ftmp1
+	fst; ftmp1	" ftmp1 += x*2
 	fld; x
-	fst; ox
+	fst; ox		" ox = x
 	fld; ftmp1
-	fst; x
+	fst; x		" x = ftmp1
 	fld; oy
 	fng
 	fad; ay
-	fst; ftmp1
+	fst; ftmp1	" ftmp1 = -oy + ay
 	fld; y
 	lac aexp
 	tad d1
 "** 12-92-119.pdf page 11
 	dac aexp
 	fad; ftmp1
-	fst; ftmp1
+	fst; ftmp1	" ftmp1 += y*2
 	fld; y
-	fst; oy
+	fst; oy		" oy = y
 	fld; ftmp1
-	fst; y
+	fst; y		" y = ftmp1
 	lac par
 	sad maxj
-	jmp i updshp
-	jms absxy
-	jms shipxy
+	jmp i updshp	" if (par == maxj) return
+	jms absxy	" absxy(par)
+	jms shipxy	" shipxy()
 	lac par
-	jms absv
+	jms absv	" absv(par)
 	fld; ox
 	fng
 	fad; x
 	fad; absx
-	fst; ox
+	fst; ox		" ox = -ox + x + absx
 	fld; oy
 	fng
 	fad; y
 	fad; absy
-	fst; oy
+	fst; oy		" oy = -oy + y + absy
 	lac maxj
-	dac par
-	jms absv
+	dac par		" par = maxj
+	jms absv	" absv(par)
 	fld; ox
 	fng
 	fad; absx
-	fst; ox
+	fst; ox		" ox = -ox + absx
 	fld; oy
 	fng
 	fad; absy
-	fst; oy
+	fst; oy		" oy = -oy + absy
 	lac par
-	jms absxy
+	jms absxy	" absxy(par)
 	fld; absx
 	fad; shipx
 	fng
-	fst; x
+	fst; x		" x = -(absx + shipx)
 	fad; ox
-	fst; ox
+	fst; ox		" ox += x
 	fld; absy
 	fad; shipy
 	fng
-	fst; y
-fadins:
+	fst; y		" y = -(absy + shipy)
+fadins:			" fad instruction
 	fad; oy
-	fst; oy
+	fst; oy		" oy += y
 	lac par
 	tad fppar
 	dac 1f
@@ -120,69 +120,69 @@ fadins:
 	dac 1f
 	fld; 1:..
 	sqrt
-	fst; rpar
-	jms dspsca
+	fst; rpar	" rpar = sqrt(prsq[par])
+	jms dspsca	" dspsca()
 	lac par
 "** 12-92-119.pdf page 12
-	jms dispname
+	jms dispname	" dispname(par)
 	jmp i updshp
 
-inscr: 0
+inscr: 0		" in screen
 	fng
-	fix
-	tad d383
+	fix		" AC = (int) -FAC
+	tad d383	" AC += 383
 	spa
-	jmp i inscr
-	tad dm768
-	sma
+	jmp i inscr	" if (AC < 0) return
+	tad dm768	" AC -= 768
+	sma		" if (AC >= 0) return
 	jmp i inscr
 	isz inscr
-	jmp i inscr
+	jmp i inscr	" return AC
 
 absxy: 0
 	sna
-	jmp 7f
+	jmp 7f		" if (AC) {
 	lmq
 	lac fldins
-	dac 2f-1
-	dac 4f-1
+	dac 2f-1	" C(2f-1) = fld
+	dac 4f-1	" C(4f-1) = fld
 	lacq
-1:
-	dac absi
+1:			" while (true) {
+	dac absi	" absi = AC
 	sna
-	jmp i absxy
+	jmp i absxy	" if (!absi) return
 	tad fppar
 	dac 9f+t
 	lac i 9f+t
 5:
 	tad px
 	dac 2f
-fldins:
+fldins:			" fld instruction
 	fld; absx
 	fad; 2:..
-	fst; absx
+	fst; absx	" if (i==0) absx = px[absi]; else absx += px[absi]
 	lac i 9f+t
 6:
 	tad py
 	dac 4f
 	fld; absy
 	fad; 4:..
-	fst; absy
+	fst; absy	" if (i==0) absy = py[absi]; else absy += py[absi]
 	lac fadins
 	dac 2b-1
 	dac 4b-1
 	lac absi
 	tad ppar
 	dac 9f+t
-	lac i 9f+t
-	jmp 1b
-7:
-	dzm absx
+	lac i 9f+t	" AC = ppar[absi]
+	jmp 1b		" }
+7:			" } else {
+	dzm absx	" absx = 0
 	dzm absx+1
 	dzm absx+2
-	dzm absy
+	dzm absy	" absy = 0
 	dzm absy+1
 	dzm absy+2
-	jmp i absxy
+	jmp i absxy	" }
 "** 12-92-119.pdf page 13
 	t = t+1
